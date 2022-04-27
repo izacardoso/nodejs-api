2e54849a3dd5f877872083c9ffcf2d410904f94c
Izacardoso <iza.cardoso129@gmail.com>
2022-04-27 11:04:39 -0300

    api agenda petshop

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,14 +1,14 @@
 const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => {
-        Atendimento.lista(res)
-    })
-
     app.get('/atendimentos/:id', (req, res) => {
         const id = parseInt(req.params.id)
 
         Atendimento.buscaPorId(id, res)
+    })
+
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res)
     })
 
     app.post('/atendimentos', (req, res) => {
